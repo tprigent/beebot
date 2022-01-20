7e85ba01853e157d431240c754904b2f8fe482d1
Théo Prigent <dev@tprigent.fr>
2022-01-20 16:09:39 +0100

    SigFox API: message & timestamp extracted

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 const request = require('request');
-const data = require('./APIroutes');
+const constants = require('./APIroutes');
 
-const url = "https://" + data.APIlogin + ":" + data.APIpassword + "@" + data.messageRoute;
+const url = "https://" + constants.APIlogin + ":" + constants.APIpassword + "@" + constants.messageRoute;
 
 request(
     {
         url : url
     },
-    function (error, response, body) {
-        console.log(body);
+    function (error, res, body) {
+        if (res.statusCode === 200) {
+            try {
+                const data = JSON.parse(body);
+
+                for(const it of data.data){
+                    const date = new Date(it.time);
+                    const parsedDate = date.getDay() + "/" + date.getMonth()+1 + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+                    console.log(parsedDate + " - " + hex_to_ascii(it.data));
+                }
+
+            } catch (e) {
+                console.log('Error parsing JSON!');
+            }
+        } else {
+            console.log('Status:', res.statusCode);
+        }
+
     }
 );
+
+function hex_to_ascii(str1)
+{
+    var hex  = str1.toString();
+    var str = '';
+    for (var n = 0; n < hex.length; n += 2) {
+        str += String.fromCharCode(parseInt(hex.substr(n, 2), 16));
+    }
+    return str;
+}
