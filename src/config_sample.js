2268f8f4842fcc0bd6bd3260f11a1744883a32fb
Théo Prigent <dev@tprigent.fr>
2023-08-15 16:14:44 +0200

    Update sample configuration file to new version

--- src/config_sample.js
+++ src/config_sample.js
@@ -1,25 +1,30 @@
 // SigFox config
-const sigfoxAPI = 'api.sigfox.com/v2/';
-const deviceID = '001DAE67'; // format XXXXXXXX, unique id of your SigFox module
-const messageRoute = `${sigfoxAPI}devices/${deviceID}/messages`;
+const sigfoxParams = {
+  apiEndpoint: 'api.sigfox.com/v2/',
+  apiLogin: 'xyz',
+  apiPassword: 'xyz',
+  devices: [
+    {
+      name: 'your device name',
+      id: 'XXXXXX',
+      location: 'Somewhere, 🇪🇺',
+    },
+  ],
+};
 
-const APIlogin = '';
-const APIpassword = '';
-
-const url = `https://${APIlogin}:${APIpassword}@${messageRoute}`;
-
-// Discord config
-const botToken = '';
-const channelName = 'alertes'; // channel where notifications will be send
-const debugChannelName = 'alertes-debug'; // in order not to pollute the main channel
+const discordParams = {
+  botToken: 'xyz',
+  alertChannelID: 'xyz',
+  debugAlertChannelID: 'xyz',
+};
 
 // Bot config
-const frequency = 5; // update frequency (in seconds)
+const frequency = 5;
 const debug = true;
 
-module.exports.url = url;
-module.exports.botToken = botToken;
-module.exports.channelName = channelName;
-module.exports.debugChannelName = debugChannelName;
-module.exports.frequency = frequency;
-module.exports.debug = debug;
+module.exports = {
+  sigfoxParams,
+  discordParams,
+  frequency,
+  debug,
+};
